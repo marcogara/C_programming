b04ef05d95348aefda25f2c52ac2ff364e82edc6
marcogara <103924910+marcogara@users.noreply.github.com>
2023-04-07 11:39:00 +0200

    Add files via upload
    Aufgabe Beschreibung ist im Quellcodekommentar

---
+++ tag04/A_01_04_04.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <windows.h>
+
+/*
+Das Programm soll entscheiden, ob ein Prüfling mindestens die Hälfte der maximalen Gesamtpunktzahl erreichte (und die Prüfung bestanden hat) oder ob es weniger Punkte waren (und er durchfiel).
+Zu diesem Zweck werden Gesamtpunktzahl und erreichte Punktzahl abgefragt.
+Anschließend soll der erreichte Prozentsatz ausgegeben werden, verbunden mit dem entsprechenden Zusatz „bestanden“ (oder „nicht bestanden“).
+Daraufhin endet das Programm.
+*/
+
+int main(void)
+{
+    system("chcp.com 1252");
+    system("cls");
+
+        float gs,ps,pw;  // Grundsatz, prozentsatz, prozentwert
+        
+        printf("Geben Sie bitte dei Maximal erreichbare Punktzahl ein: ");
+        fflush(stdin);
+        scanf("%f",&gs);
+
+        printf("Geben Sie bitte die erreichte Punktzahl ein: ");
+        fflush(stdin);
+        scanf("%f",&ps);
+        printf("\n");
+
+        pw=ps/gs*100;
+        gs=gs/2;
+        //  printf("\npw ist: %f",pw);
+        //  printf("\ngs ist: %f",gs);
+
+        if (pw>gs)
+        {
+            printf("Sie haben %.f Prozent erreicht und bestanden",pw);
+        } 
+        else
+        {
+            printf("Sie haben %.f Prozent erreicht und nicht bestanden",pw);
+        }
+
+        printf("\nProgramm endet nun.");
+
+
+    printf("\n\n\n");
+    system("pause");
+}
